449e6c2c9b9e25c7562bf6907b79d5babe872b79
Marcela Silva <marcela.ops@hotmail.com>
2023-12-26 11:32:18 -0300

    deleting

--- nest/nestjs-rest-loja-aula-2/src/usuario/usuario.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsuarioController } from './usuario.controller';
-import { UsuarioRepository } from './usuario.repository';
-@Module({
-  controllers: [UsuarioController],
-  providers: [UsuarioRepository],
-})
-export class UsuarioModule {}
