c094ed0b4294cc0aad77b1ac7122ba5555541c4a
Marcela Silva <marcela.ops@hotmail.com>
2024-01-04 14:25:02 -0300

    feat: first components

--- angular/memoteca/src/app/app-routing.module.ts
+++ angular/memoteca/src/app/app-routing.module.ts
@@ -6,5 +6,8 @@ const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  /* template: `
+    <h1>vc consegue escrever na págica por aqui</h1>
+  `, */
 })
 export class AppRoutingModule { }
