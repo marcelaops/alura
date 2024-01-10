beccd52e9626345c73c04535ed944e62811404b1
Marcela Silva <marcela.ops@hotmail.com>
2024-01-10 16:55:24 -0300

    reactive forms, validators and errors

--- angular/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,12 +9,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
-
-  pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
 
   formulario!: FormGroup;
 
@@ -26,19 +20,34 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulario reativo'],
-      autoria: [''],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])], 
+      // regex
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => this.router.navigate([['/listarPensamento']]))
-    // a rota fornecida estará dentro de um Event Binding.
+    console.log(this.formulario.get('autoria')?.errors);
+
+    if(this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => this.router.navigate(['/listarPensamento']))
+      // a rota fornecida estará dentro de um Event Binding.
+    }
   }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
+  }
 
+  habilitarBotao() {
+    if(this.formulario.valid) return 'botao';
+    else return 'botao__desabilitado';
   }
 }
