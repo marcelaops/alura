f3938eb6079a7e9f84a3e13050fee07a035a98dc
Marcela Silva <marcela.ops@hotmail.com>
2024-08-30 12:30:54 -0300

    fix: excluindo o git p clonar novamente

--- nest/loja3/src/pedido/dto/AtualizaPedido.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { StatusPedido } from '../enum/statuspedido.enum';
-
-export class AtualizaPedidoDto {
-  @IsEnum(StatusPedido)
-  status: StatusPedido;
-}
