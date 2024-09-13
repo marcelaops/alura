eebab802978794fa7d23419d431f495bb5edb805
Marcela Silva <marcela.ops@hotmail.com>
2024-09-13 10:55:20 -0300

    feat: i o bcrypt e usei o bcrypt.hash p rashear a senha

--- nest/nestjs-curso-3-aula-5/src/recursos/pipes/hashear-senha.pipe.ts
+++ nest/nestjs-curso-3-aula-5/src/recursos/pipes/hashear-senha.pipe.ts
@@ -1,8 +1,15 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class HashearSenhaPipe implements PipeTransform {
-  transform(senha: string) {
-    return senha + 'abcdef';
+  constructor(private configService: ConfigService) {}
+
+  async transform(senha: string) {
+    const sal = this.configService.get<string>('SAL_SENHA');
+    const senhaHasheada = await bcrypt.hash(senha, sal!);
+
+    return senhaHasheada;
   }
 }
