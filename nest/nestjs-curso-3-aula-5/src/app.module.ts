2947fc9ecba82530d2b82cc4c0ee44f5eab56c82
Marcela Silva <marcela.ops@hotmail.com>
2024-09-23 13:20:42 -0300

    feat: Logando erros para produção

--- nest/nestjs-curso-3-aula-5/src/app.module.ts
+++ nest/nestjs-curso-3-aula-5/src/app.module.ts
@@ -1,4 +1,8 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ConsoleLogger,
+  Module,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -43,6 +47,7 @@ import { AutenticacaoModule } from './modulos/autenticacao/autenticacao.module';
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
+    ConsoleLogger,
   ],
 })
 export class AppModule {}
--- nest/nestjs-curso-3-aula-5/src/recursos/filtros/filtro-de-excecao-global.ts
+++ nest/nestjs-curso-3-aula-5/src/recursos/filtros/filtro-de-excecao-global.ts
@@ -1,6 +1,7 @@
 import {
   ArgumentsHost,
   Catch,
+  ConsoleLogger,
   ExceptionFilter,
   HttpException,
   HttpStatus,
@@ -9,10 +10,14 @@ import { HttpAdapterHost } from '@nestjs/core';
 
 @Catch()
 export class FiltroDeExcecaoGlobal implements ExceptionFilter {
-  constructor(private adapterHost: HttpAdapterHost) {}
+  constructor(
+    private adapterHost: HttpAdapterHost,
+    private loggerNativo: ConsoleLogger,
+  ) {}
 
   catch(excecao: unknown, host: ArgumentsHost) {
-    console.log(excecao);
+    this.loggerNativo.error(excecao);
+    console.error(excecao);
 
     const { httpAdapter } = this.adapterHost;
 
